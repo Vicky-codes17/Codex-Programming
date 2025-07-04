d09d2a3c221e36fd3fbf56729a7057454c3388c0
Vicky-codes17 <rameshvignesh0706@gmail.com>
2025-07-04 15:57:46 +0530

    added file: Python/snail_mail.py

---
+++ Python/snail_mail.py
@@ -0,0 +1,6 @@
+#Letter to myself 
+print('Friday 4 July')
+print('😊 I am feeling Good')
+print('💻 By Accomplish by learning to code work on mini projects')
+print('Hi I am Happy to do the coding and exicting to encahnce my technical skills')
+print('My Favorite emoji : 🎨')
