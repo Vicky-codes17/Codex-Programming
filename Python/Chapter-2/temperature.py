bcaa21f6ef4f05d4c748efcf2ff7667c92d6ff02
Vicky-codes17 <rameshvignesh0706@gmail.com>
2025-07-04 16:07:16 +0530

    added: temperature.py

---
+++ Python/Chapter-2/temperature.py
@@ -0,0 +1,9 @@
+#Fahrenheit to Celsius
+
+fahrenheit = float(input("Enter the temparature of Fahrenheit :"))
+
+#For Celsisus Calculation
+celsisus = (fahrenheit-32)/1.8
+
+#Displaying the Output
+print("The temparature in Celsisus : ",celsisus)
