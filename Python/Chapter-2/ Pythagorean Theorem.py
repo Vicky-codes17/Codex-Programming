910962051be0719e8f5e91e870beb390f23782d3
Vicky-codes17 <rameshvignesh0706@gmail.com>
2025-07-04 16:15:07 +0530

    added file: Pythagorean_Theorem.py

---
+++ Python/Chapter-2/ Pythagorean Theorem.py
@@ -0,0 +1,8 @@
+# Pythagorean theorem
+
+a = float(input("Enter the length of side a :"))
+b = float(input("Enter the length of side b :"))
+
+c = (a**2 + b**2) ** 0.5
+
+print(f"The lenght of the hyptenuse is : {c : .2f}")
