2fb079b43c3dcfd66947bf97a3f1cfde67bc19ce
Vicky-codes17 <rameshvignesh0706@gmail.com>
2025-07-04 16:04:29 +0530

    added : Python/Chapter-2/Data_Types.py

---
+++ Python/Chapter-2/Data_Types.py
@@ -0,0 +1,12 @@
+# Data Types
+name ="Vignesh Ramesh"
+user_id = 123456789
+progress = 1.20
+xp = 60
+verified = True
+
+print(type(name))
+print(type(user_id))
+print(type(progress))
+print(type(xp))
+print(type(verified))
