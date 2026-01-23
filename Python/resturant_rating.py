10300f2fef5d79d931286bcba32438792a60a539
Vignesh R <153487240+Vicky-codes17@users.noreply.github.com>
2026-01-23 16:20:24 +0530

    Added: Challenges/resturant_rating.py

---
+++ Python/resturant_rating.py
@@ -0,0 +1,12 @@
+rating = float("Enter the rating for the restaurant: ")
+
+if rating > 4.5:
+    print("Extraordinary")
+elif rating > 4:
+    print("Excellent")
+elif rating > 3:
+    print("Good")
+elif rating > 2:
+    print("Fair")
+else:
+    print("Poor")
